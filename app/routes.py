0789aa22d15ee8654877db33e55e5a503c320906
Somberlord <somberlord@gmail.com>
2025-06-25 23:04:43 +0200

    Update footer

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,32 @@ import app.page_loader as page_loader
 def index():
     return page_loader.render_guide(page_loader.DEFAULT_LANGUAGE, 'home')
 
+@app.route('/thanks')
+def thanks():
+    return thanks_lang(page_loader.DEFAULT_LANGUAGE)
+
+@app.route('/<lang>/thanks')
+def thanks_lang(lang):
+    return render_template(f'lang/{lang}/thanks.html', title='Thanks', navlist=page_loader.get_pages(lang))
+
+@app.route('/contact')
+def contact():
+    return contact_lang(page_loader.DEFAULT_LANGUAGE)
+
+@app.route('/<lang>/contact')
+def contact_lang(lang):
+    return render_template(f'lang/{lang}/contact.html', title='Contact', navlist=page_loader.get_pages(lang))
+
+@app.route('/legal')
+def legal():
+    return legal_lang(page_loader.DEFAULT_LANGUAGE)
+
+@app.route('/<lang>/legal')
+def legal_lang(lang):
+    return render_template(f'lang/{lang}/legal.html', title='Legal', navlist=page_loader.get_pages(lang))
+
+
+
 @app.route('/<page_name>')
 def page(page_name):
     return page_loader.render_guide(page_loader.DEFAULT_LANGUAGE, page_name)
